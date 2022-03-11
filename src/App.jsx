11313fd72f29b19a64e0a76b378d43068ef18652
André Luis <anddreluis98@gmail.com>
2022-03-11 14:57:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import react from 'react'
+
+export default function App() {
+
+  return <h1>salve</h1>
+}
+   
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,10 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+
+            </ul>
+        </section>
+    )
+}
